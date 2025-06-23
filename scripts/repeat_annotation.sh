ebcb4f74e3c32ade3c0a1a0470651fa1a67a10b5
Jiangjiangzhang6 <101324838+Jiangjiangzhang6@users.noreply.github.com>
2025-06-23 08:36:00 +0800

    Update repeat_annotation.sh
    repeat annotation in this research

--- scripts/repeat_annotation.sh
+++ scripts/repeat_annotation.sh
@@ -13,5 +13,12 @@ source activate EDTA2
 RepeatMasker -pa 10 -e ncbi -lib consensi.fa.classified hap1.fa  -gff -xsmall
 ### the fine repeat annotaion
 mkdir 01.deepte.anno; cd 01.deepte.anno
-perl extract_catergoy.pl ###
-perl 2.pl | perl 4.pl - > opt_DeepTE.fasta2
+perl extract_catergoy.pl ### extract the unknown and annotation element from the library
+###annotation the unknown element
+python /public/home/zhangjiangjiang/software/DeepTE-master/DeepTE.py \
+    -d working_dir \
+    -i un.fa \
+    -m_dir ~/jjzhang/yan/genome/hemp/03.changsha_genome/01.hap1/04.repeatmasker/03.anno_further1_deepte/Plants_model -sp P
+perl 2.pl | perl 4.pl - > opt_DeepTE.fasta2 ### handle the deepte annotation
+cat opt_DeepTE.fasta2 no_un.fa |seqkit seq -w 100 > deepte_an.fa ### merge the unknown element annotation by deepte and the raw annotaion file library
+RepeatMasker -pa 20 -e ncbi -lib deepte_an.fa fa1  -gff -xsmall ### genome repeat annotation again. and the comparable the two results
